6f6499bd220fb01862f264e40c4f97ac22ae7904
JamesRaff03 <102355336+JamesRaff03@users.noreply.github.com>
2022-11-11 16:19:09 +0000

    Update distributor.go

--- gol/distributor.go
+++ gol/distributor.go
@@ -13,21 +13,22 @@ type distributorChannels struct {
 func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
+	Image := <-c.ioInput
+	Filename := <-c.ioFilename
+	world := [p.ImageWidth][p.ImageHeight]uint8
 
 	turn := 0
-
 
 	// TODO: Execute all turns of the Game of Life.
 
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
 	c.events <- StateChange{turn, Quitting}
-	
+
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
 }

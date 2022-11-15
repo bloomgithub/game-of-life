df10e67f7b3380a69d99a1c48a53628babacdbd9
bloomgithub <89202906+bloomgithub@users.noreply.github.com>
2022-11-15 20:04:50 +0000

    Update distributor.go

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,7 +1,5 @@
 package gol
 
-import "fmt"
-
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -17,7 +15,7 @@ func distributor(p Params, c distributorChannels) {
 	// TODO: Create a 2D slice to store the world.
 
 	// workout filename from the parameters coming in
-	fileName := string(p.ImageWidth) + "x" + string(p.ImageHeight) + "pgm"
+	fileName := string(p.ImageWidth) + "x" + string(p.ImageHeight)
 
 	//send filename down appropriate channel
 	c.ioFilename <- fileName
@@ -28,8 +26,6 @@ func distributor(p Params, c distributorChannels) {
 	row2 := make([]uint8, p.ImageWidth)
 	world = append(world, row1)
 	world = append(world, row2)
-
-	fmt.Print(world)
 
 	turn := 0
 
